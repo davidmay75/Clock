3f9bf1a74e59213d4257c8b9071b33c5a1e322c3
David May <davidmaybmx@gmail.com>
2019-11-05 22:00:05 -0500

    Added digital clock

--- script.js
+++ script.js
@@ -5,19 +5,28 @@ const minutehand = document.querySelector('[data-minute-hand]')
 const secondhand = document.querySelector('[data-second-hand]')
 const datewindow = document.querySelector('[data-date-window]')
 
+const digHour = document.querySelector('[data-dig-hour]')
+const digMinute = document.querySelector('[data-dig-minute]')
+const digSecond = document.querySelector('[data-dig-second]')
+
 function setClock(){
     const currentDate = new Date()
-    const date = currentDate.getUTCDate()
+    const date = currentDate.getUTCDate()//needs time zones
     const secondsRatio = currentDate.getSeconds() / 60
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+
+    datewindow.innerHTML = date
 
     setRotation(secondhand, secondsRatio)
     setRotation(minutehand, minutesRatio)
     setRotation(hourhand, hoursRatio)
 
-    datewindow.innerHTML = date
-    console.log(date)
+    //DIGITAL CLOCK
+    
+    digSecond.innerHTML = Math.floor(currentDate.getSeconds())
+    digMinute.innerHTML = currentDate.getMinutes()
+    digHour.innerHTML = currentDate.getHours()
 }
 
 function setRotation(element, rotationRatio){
